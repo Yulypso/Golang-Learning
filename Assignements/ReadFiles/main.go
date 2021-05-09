0ff21b52e3ab6fa77e5a41b248f2c8ee45756816
Yulypso <khamphousone.thierry@outlook.fr>
2021-05-09 19:11:32 +0200

    add assignement3

---
+++ Assignements/ReadFiles/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	io.Copy(os.Stdout, file)
+}
